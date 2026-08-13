9eb5a16bc3bf41c7e9265f12ddd4db2d9b9944b2
here give your github username <here give your github registered email>
2026-08-13 12:40:24 +0530

    Notes-Keeper-Smart-Notes-Organizer-Backend

--- server/main.js
+++ server/main.js
@@ -32,9 +32,7 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(
-  cors()
-);
+app.use(cors());
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
