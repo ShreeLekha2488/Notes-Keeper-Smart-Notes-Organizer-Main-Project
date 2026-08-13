1d1e01095b5f693dfe9fc2924dd28e39c9fb11a4
here give your github username <here give your github registered email>
2026-08-13 11:32:54 +0530

    removed cors

--- server/main.js
+++ server/main.js
@@ -33,10 +33,7 @@ connectDB();
 
 // Middleware
 app.use(
-  cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
-    credentials: true,
-  })
+  cors()
 );
 
 app.use(express.json({ limit: "10mb" }));
