4be189af176e223da6d545283c6a9e8bfc4ca9df
here give your github username <here give your github registered email>
2026-08-13 14:31:00 +0530

    updated my main project

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
+  baseURL: "https://notes-keeper-smart-notes-organizer.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
